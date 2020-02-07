9eb2fc407353a427bdd64430144b258af63ef801
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2020-02-07 17:35:21 +0200

    Edit roducts and services page

--- lib/UI/Products.dart
+++ lib/UI/Products.dart
@@ -85,16 +85,16 @@ class _ProductsState extends State<Products> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
         ),
         Padding(
-          padding: EdgeInsets.only(left:10,top: 5),
+          padding: EdgeInsets.only(left:0,top: 5),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("1-Shops",style: TextStyle(fontSize: 15,)),
-              Text("2-Wholesale",style: TextStyle(fontSize: 15,)),
-              Text("3-Retailers",style: TextStyle(fontSize: 15,)),
-              Text("4-Supermarkets",style: TextStyle(fontSize: 15,)),
-              Text("5-Malls",style: TextStyle(fontSize: 15,)),
+              rowItem("1-Shops"),
+              rowItem("2-Wholesale"),
+              rowItem("3-Retailers"),
+              rowItem("4-Supermarkets"),
+              rowItem("5-Malls"),
             ],
           ),
         ),
@@ -111,22 +111,65 @@ class _ProductsState extends State<Products> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
         ),
         Padding(
-          padding: EdgeInsets.only(left:10,top: 5),
+          padding: EdgeInsets.only(left:0,top: 5),
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("1-Health care and centers",style: TextStyle(fontSize: 15,)),
-              Text("2-Apparel designers",style: TextStyle(fontSize: 15,)),
-              Text("3-Craftsmen",style: TextStyle(fontSize: 15,)),
-              Text("4-Repairers",style: TextStyle(fontSize: 15,)),
-              Text("5-Beauty Clinics",style: TextStyle(fontSize: 15,)),
-              Text("6-Haircut Saloons",style: TextStyle(fontSize: 15,)),
-              Text("7-Bakers"),
+              // Text("1-Health care and centers",style: TextStyle(fontSize: 15,)),
+              // Text("2-Apparel designers",style: TextStyle(fontSize: 15,)),
+              // Text("3-Craftsmen",style: TextStyle(fontSize: 15,)),
+              // Text("4-Repairers",style: TextStyle(fontSize: 15,)),
+              // Text("5-Beauty Clinics",style: TextStyle(fontSize: 15,)),
+              // Text("6-Haircut Saloons",style: TextStyle(fontSize: 15,)),
+              // Text("7-Bakers"),
+              rowItem("1-Health care and centers"),
+              rowItem("2-Apparel designers"),
+              rowItem("3-Craftsmen"),
+              rowItem("4-Repairers"),
+              rowItem("5-Beauty Clinics"),
+              rowItem("6-Haircut Saloons"),
+              rowItem("7-Bakers"),
             ],
           ),
         ),
       ],
+    );
+  }
+
+   Widget rowItem(String text) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8, bottom: 8, left: 15, right: 15),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, '/shops');
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              text,
+              style: TextStyle(fontSize: 15, color: Color(0xff515C6F)),
+            ),
+            Container(
+              //width: 25,
+              height: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 10,
+                  color: Color(0xff0dbea8),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
